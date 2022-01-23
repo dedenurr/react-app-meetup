55a4108fd57bbe244f555297022a9d9734158591
dedenurr <dedenurrahman.git@gmail.com>
2022-01-23 20:44:52 +0700

    add layout & some Pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { Route, Switch } from 'react-router-dom';
+import MainNavigation from './components/layout/MainNavigation';
+import AllMeetupsPage from './pages/AllMeetups';
+import FavoritesPage from './pages/Favorites';
+import NewMeetupsPage from './pages/NewMeetups';
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <MainNavigation />
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupsPage />
+        </Route>
+        <Route path="/favorites">
+          <FavoritesPage />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
